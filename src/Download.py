6c1e5bdfe33b04bb9602a17b5091037ae5703f96
zxqlxy <liuxinyun6@yahoo.com>
2020-02-08 22:59:18 -0600

    Finished processing following paper

--- src/Download.py
+++ src/Download.py
@@ -8,17 +8,17 @@ months = list(map(lambda i: '0'+ i if len(i) == 1 else i, months))
 days = [str(i + 1) for i in range(31)]
 days = list(map(lambda i: '0'+ i if len(i) == 1 else i, days))
 urlBase = "http://jsoc.stanford.edu/data/aia/synoptic"
-locPath = "/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/High Temperature Extraction/data/SDOAIA"
+locPath = "/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data"
 
 for yr in years:
     for mo in months:
         print(mo)
         for da in days:
-            thisFile = "AIA"+ yr + mo + da + "_0000_0094.fits"
+            thisFile = "AIA"+ yr + mo + da + "_0000_0193.fits"
             thisPath = "/".join([yr,mo,da,"H0000"])
             print(thisFile)
 
-            myUrl = "/".join([urlBase,thisPath,thisFile ])
+            myUrl = "/".join([urlBase,thisPath,thisFile])
             myDest = "/".join([locPath, thisFile])
             # print(myDest)
             print(myUrl)
--- src/process.py
+++ src/process.py
@@ -1,8 +1,38 @@
 import sunpy.io
 import sunpy.map
 import sunpy.data.sample
+from sunpy.map.maputils import all_coordinates_from_map
+import matplotlib.pyplot as plt
 
-aia =  sunpy.io.fits.read("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/High Temperature Extraction/data/SDOAIA/AIA20190308_0000_0094.fits")
-smap = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/High Temperature Extraction/data/SDOAIA/AIA20190308_0000_0094.fits")
-smap.peek(draw_limb=True)
-# print(sunpy.data.sample.AIA_171_IMAGE)
+import numpy as np
+lis =  sunpy.io.fits.read("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/094/AIA20190308_0000_0094.fits")
+smap094 = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/094/AIA20190308_0000_0094.fits")
+smap171 = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/171/AIA20190308_0000_0171.fits")
+smap193 = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/193/AIA20190308_0000_0193.fits")
+
+print(lis)
+# smap094.peek(draw_limb=True)
+# smap171.peek(draw_limb=True)
+# smap193.peek(draw_limb=True)
+# # print(sunpy.data.sample.AIA_171_IMAGE)
+
+
+a1 = -7.31*10**(-2)
+a2 = -9.75*10**(-1)
+a3 = -9.90*10**(-2)
+a4 = -2.84*10**(-3)
+f = 0.31
+hpc_coords = all_coordinates_from_map(smap094)
+# print(np.array(smap094))
+# print(smap094)
+# print(type(smap094))
+# print(hpc_coords)
+inside = (f * smap171.data) + ((1-f) * smap193.data)/116.54
+smap = 0.39 * (a1 * inside ** 1 + a2 * inside ** 2 + a3 * inside ** 3 + a4 * inside ** 4)
+
+data,header = lis[1]
+print(data)
+mymap = sunpy.map.Map(data - smap, header)
+mymap.peek(draw_limb=True)
+im = mymap.plot()
+
