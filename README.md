9a4ff63d12a1e1cb8f0167beefd94738aa3de8c4
zxqlxy <liuxinyun6@yahoo.com>
2020-02-09 00:21:44 -0600

    Finished processing following paper

---
+++ README.md
@@ -0,0 +1,12 @@
+# High Temperature Solar EUV Signal Extraction and Restorion
+
+## Preprocess the data to get Fe_XVIII signal
+The method is from [A Systematic Survey of High-Temperature Emission in Solar Active Regions](http://dx.doi.org/10.1088/0004-637X/759/2/141) to extract Fe_XVIII signal on 94 by subtracting "warm" signal from 171 and 193.
+The data is from [Standford JSOC Database](http://jsoc.stanford.edu/data/aia/synoptic/)
+
+The result of a Fe_XVIII signal full disk [solar picture](pics/20190309_Fe_XVIII.jpg) is obtained from [94 image](pics/20190309_0000_0094.jpg), [171 image](pics/20190309_0000_0171.jpg), [193_image](pics/20190309_0000_0193.jpg).
+
+![solar picture](pics/20190309_Fe_XVIII.jpg)
+![94 image](pics/20190309_0000_0094.jpg)
+![171 image](pics/20190309_0000_0171.jpg)
+![193_image](pics/20190309_0000_0193.jpg)
--- src/process.py
+++ src/process.py
@@ -4,35 +4,63 @@ import sunpy.data.sample
 from sunpy.map.maputils import all_coordinates_from_map
 import matplotlib.pyplot as plt
 
-import numpy as np
-lis =  sunpy.io.fits.read("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/094/AIA20190308_0000_0094.fits")
-smap094 = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/094/AIA20190308_0000_0094.fits")
-smap171 = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/171/AIA20190308_0000_0171.fits")
-smap193 = sunpy.map.Map("/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/193/AIA20190308_0000_0193.fits")
-
-print(lis)
-# smap094.peek(draw_limb=True)
-# smap171.peek(draw_limb=True)
-# smap193.peek(draw_limb=True)
-# # print(sunpy.data.sample.AIA_171_IMAGE)
-
+base = "/Users/lxy/Desktop/Rice/PHYS 491 & 493 Research/data/"
+years = ["2019"]
+months = [str(i+ 1) for i in range(12)]
+months = list(map(lambda i: '0'+ i if len(i) == 1 else i, months))
+days = [str(i + 1) for i in range(31)]
+days = list(map(lambda i: '0'+ i if len(i) == 1 else i, days))
 
 a1 = -7.31*10**(-2)
 a2 = -9.75*10**(-1)
 a3 = -9.90*10**(-2)
 a4 = -2.84*10**(-3)
 f = 0.31
-hpc_coords = all_coordinates_from_map(smap094)
-# print(np.array(smap094))
-# print(smap094)
-# print(type(smap094))
-# print(hpc_coords)
-inside = (f * smap171.data) + ((1-f) * smap193.data)/116.54
-smap = 0.39 * (a1 * inside ** 1 + a2 * inside ** 2 + a3 * inside ** 3 + a4 * inside ** 4)
 
-data,header = lis[1]
-print(data)
-mymap = sunpy.map.Map(data - smap, header)
-mymap.peek(draw_limb=True)
-im = mymap.plot()
+def process():
+    """
 
+    """
+    for yr in years:
+        for mo in months:
+            for da in days:
+                try:
+                    lis = sunpy.io.fits.read(
+                        base + "094/AIA" + yr + mo + da + "_0000_0094.fits")
+                    smap171 = sunpy.map.Map(
+                        base + "171/AIA" + yr + mo + da + "_0000_0171.fits")
+                    smap193 = sunpy.map.Map(
+                        base + "193/AIA" + yr + mo + da + "_0000_0193.fits")
+
+                    # formula comes from appendix of doi:10.1088/0004-637X/759/2/141
+                    inside = (f * smap171.data) + ((1-f) * smap193.data)/116.54
+                    smap = 0.39 * (a1 * inside ** 1 + a2 * inside ** 2 + a3 * inside ** 3 + a4 * inside ** 4)
+                    data,header = lis[1]
+
+                    mymap = sunpy.map.Map(data - smap, header)
+                    # mymap.peek(draw_limb=True)
+                    im = mymap.plot()
+                    plt.savefig(base + yr + mo + da + "_Fe_XVIII.jpg")
+                except FileNotFoundError:
+                    pass
+
+
+def plot_day(yr:str , mo:str, da:str):
+    """
+
+    :param yr:
+    :param mo:
+    :param da:
+    """
+    lis = sunpy.map.Map(
+                        base + "094/AIA" + yr + mo + da + "_0000_0094.fits")
+    im = lis.plot()
+    plt.savefig(base + yr + mo + da + "_0000_0094.jpg")
+    smap171 = sunpy.map.Map(
+                        base + "171/AIA" + yr + mo + da + "_0000_0171.fits")
+    im = smap171.plot()
+    plt.savefig(base + yr + mo + da + "_0000_0171.jpg")
+    smap193 = sunpy.map.Map(
+                        base + "193/AIA" + yr + mo + da + "_0000_0193.fits")
+    im = smap193.plot()
+    plt.savefig(base + yr + mo + da + "_0000_0193.jpg")
